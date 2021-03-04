8521abad73a41e8e36f90a6e11bb6af7b8710634
MediocreMilton <57580428+mediocremilton@users.noreply.github.com>
2021-03-04 15:47:52 -0500

    Update TapeBotUnit.lua

--- ScrapMechanic/Survival/Scripts/game/units/TapeBotUnit.lua
+++ ScrapMechanic/Survival/Scripts/game/units/TapeBotUnit.lua
@@ -705,7 +705,7 @@ function TapebotUnit.sv_spawnParts( self, impact, headDestroyed )
 	local bodyOffset = bodyRot * sm.vec3.new( -0.25, 0.25, 0.375 )
 	bodyPos = bodyPos - bodyOffset
 
-	if math.random( 1, 8 ) == 1 then
+	if math.random( 1, 5 ) == 1 then
 	local headBody = sm.body.createBody( bodyPos, bodyRot, true )
 	local headShape = headBody:createPart( obj_robotparts_tapebothead01, sm.vec3.new( 0, 1, 2 ), sm.vec3.new( 0, 1, 0 ), sm.vec3.new( -1, 0, 0 ), true )
 		if headDestroyed == true then
